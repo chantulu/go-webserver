20b3e395bcc7f54d6230d63b41e0f7a6aeaea5f1
chantulu <gluisv9@gmail.com>
2024-08-09 14:17:18 -0700

    added health monitoring

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 )
 
+func HealzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("./static"))
-	mux.Handle("/", fs)
+	fileServer := http.FileServer(http.Dir("./static"))
+	mux.Handle("/app/*", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", HealzHandler)
 	server := http.Server{Handler: mux, Addr: "localhost:8080"}
 	log.Fatal(server.ListenAndServe())
 }
